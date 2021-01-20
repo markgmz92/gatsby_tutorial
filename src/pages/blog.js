36735bb9bb74582520105ce68a43fccdfff4f0da
markgmz92 <markgmz92@gmail.com>
2021-01-20 19:42:56 +0800

    [GP - 002] Created link component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,12 @@
 import React from "react"
-
+import { Link } from "gatsby"
 const blog = () => {
   return (
     <div>
       <h1>This is our blog page</h1>
+      <div>
+        <Link to="/">Home Page</Link>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default () => {
+  return (
+    <div>
+      Hello people!
+      <div>
+        <Link to="/blog/">blog page</Link>
+      </div>
+      <a href="https://www.gatsbyjs.org">Gatsby docs</a>
+    </div>
+  )
 }

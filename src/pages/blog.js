aaba7445bb132d695439f7b42bcca11a315ee60a
markgmz92 <markgmz92@gmail.com>
2021-01-22 18:21:29 +0800

    [GP - 006] Included styled component button

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 const blog = () => {
   return (
     <Layout>
       <h1>This is our blog page</h1>
+      <p>Lorem25</p>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
 
 const index = () => {
   return (
     <Layout>
       <h1> Hello from Gatsby</h1>
+      <ExampleButton>Click Me</ExampleButton>
     </Layout>
   )
 }
